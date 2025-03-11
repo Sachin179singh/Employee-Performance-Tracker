03c040da84769b6bdf984e0eb2440bf93a39b1d9
sujeet kumar <kumarsujeet95285@gmail.com>
2025-03-11 10:28:04 +0530

    login page

--- first.py
+++ first.py
@@ -13,14 +13,14 @@ def index():
 @app.route('/about')
 def about():
     return render_template('about.html')
+@app.route('/login')
+def login():
+    return render_template('login.html')
+@app.route('/register')
+def register():
+    return render_template('register.html')
 
-@app.route('/form', methods=['GET','POST'])
-def forminput():
-    return render_template('form.html')
 
-@app.route('/results', methods=['GET','POST'])
-def result():
-    return render_template('results.html')
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000, debug=True)
